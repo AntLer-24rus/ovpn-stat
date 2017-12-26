b44f7c26274ab045302aa834845cd02686b4630b
AntLer-24rus <satushev@gmail.com>
2017-12-26 15:43:56 +0700

    add code

---
+++ libs/logger.js
@@ -0,0 +1,23 @@
+const winston = require('winston');
+const path = require('path');
+
+const ENV = process.env.NODE_ENV;
+
+function getLogger(module) {
+  const pathToModule = module.filename
+    .split(path.sep)
+    .slice(-2)
+    .join(path.sep);
+
+  return new winston.Logger({
+    transports: [
+      new winston.transports.Console({
+        colorize: true,
+        level: ENV === 'development' ? 'debug' : 'error',
+        label: pathToModule,
+      }),
+    ],
+  });
+}
+
+module.exports = getLogger;
