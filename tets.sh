a18ef3e1ae8770a724e1e1aed39fedad8cac7401
AntLer-24rus <satushev@gmail.com>
2018-03-02 17:22:48 +0700

    test 2

--- tets.sh
+++ tets.sh
@@ -3,7 +3,7 @@
 txtrst='\e[0m' # Text Reset
 txtred='\e[0;31m' # Red
 txtgrn='\e[0;32m' # Green
-WORK_TREE='/home/node/ovpn-stat'
+WORK_TREE='/www/ovpn-stat'
 
 echo "========================================"
 while read oldrev newrev ref
@@ -11,7 +11,7 @@ do
   case $ref in
     refs/heads/master )
 
-      git --work-tree=$WORK_TREE checkout -f dev
+      git --work-tree=$WORK_TREE checkout -f master
       if [ $? -eq 0 ]; then
         echo -e "${txtgrn}DEVELOPER SERVER successfully updated${txtrst}"
       else
