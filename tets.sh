a19f805492722b20b3d0ff6515f203650c832ecc
AntLer-24rus <satushev@gmail.com>
2018-03-02 17:17:25 +0700

    test

---
+++ tets.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+txtrst='\e[0m' # Text Reset
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+WORK_TREE='/home/node/ovpn-stat'
+
+echo "========================================"
+while read oldrev newrev ref
+do
+  case $ref in
+    refs/heads/master )
+
+      git --work-tree=$WORK_TREE checkout -f dev
+      if [ $? -eq 0 ]; then
+        echo -e "${txtgrn}DEVELOPER SERVER successfully updated${txtrst}"
+      else
+        echo -e "${txtred}Failed to checkout DEVELOPER SERVER!${txtrst}"
+      fi
+    ;;
+    * )
+    echo "NO UPDATES FOR $oldrev"
+    ;;
+  esac
+done
+echo "========================================"
